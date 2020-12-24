e18e38c761bae758340a4511a9543b8f6dca8234
rogerximenez <rogerximenez@gmail.com>
2020-12-24 12:58:35 -0600

    added solution to project

--- script.js
+++ script.js
@@ -3,4 +3,34 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+const generateTarget = () => Math.floor(Math.random() * 10)
 
+
+// Compares which value is closest to 0 t determine winner
+const compareGuesses = (userGuess, computerGuess, targetGuess) => {
+    checkValidInput(userGuess)
+    let uGuess = getAbsoluteDistance(userGuess, targetGuess)
+    let cGuess = getAbsoluteDistance(computerGuess, targetGuess)
+    return cGuess >= uGuess ? true : false;
+}
+
+// Updates the score for the computer or human
+const updateScore = winner => {
+    if(winner === 'human') {
+        humanScore += 1
+    } else if (winner === 'computer') {
+        computerScore += 1
+    } else {
+        console.log("Something went wrong")
+    }
+}
+
+const advanceRound = () => currentRoundNumber ++
+
+const getAbsoluteDistance = (x, y) => Math.abs(x - y)
+
+const checkValidInput = (x) => {
+    if(x > 9 || x < 0) {
+        alert("The User's Value is out of the range!")
+    }
+}
